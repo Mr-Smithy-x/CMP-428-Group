0e082591d82c5eb1cba692e9cc60aebba223081a
Charlton Smith <CharltonSmith@outlook.com>
2020-12-07 23:00:21 -0500

    alpha transparency with hud

--- src/groupproject/containers/zelda/contracts/MiniHealthBar.java
+++ src/groupproject/containers/zelda/contracts/MiniHealthBar.java
@@ -9,7 +9,16 @@ public interface MiniHealthBar extends Life, CameraContract {
 
     default void renderHud(Graphics graphics, BaseCamera camera) {
         if (!isDead()) {
-            graphics.setColor(new Color(150, 70, 70));
+            int x = getCameraOffsetX(camera).intValue() + getWidth().intValue();
+            //int y = getCameraOffsetY(camera).intValue() + getHeight().intValue();
+
+
+            int alpha = Math.min(Math.max(0, x), Math.min(Math.max(x, 255), 255));
+
+            Color red = new Color(150, 70, 70, alpha);
+            Color green = new Color(0, 150, 70, alpha);
+            Color black = new Color(0, 0, 0, alpha);
+            graphics.setColor(red);
             int maxWidth = (int) getMaxHealth() / 2;
             int width = (int) getHealth() / 2;
             graphics.fillRect(
@@ -17,15 +26,15 @@ public interface MiniHealthBar extends Life, CameraContract {
                     getCameraOffsetY(camera).intValue() - getHeight().intValue() / 2,
                     maxWidth,
                     5);
-            graphics.setColor(new Color(0, 150, 70));
+            graphics.setColor(green);
             graphics.fillRect(
                     getCameraOffsetX(camera).intValue() - width / 8,
                     getCameraOffsetY(camera).intValue() - getHeight().intValue() / 2,
                     width,
                     5);
-            graphics.setColor(Color.BLACK);
+            graphics.setColor(black);
             graphics.drawRect(
-                    getCameraOffsetX(camera).intValue() - width/8,
+                    getCameraOffsetX(camera).intValue() - width / 8,
                     getCameraOffsetY(camera).intValue() - getHeight().intValue() / 2,
                     maxWidth,
                     5);
